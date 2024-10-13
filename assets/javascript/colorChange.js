24ed5e968daf6fde4edf2bcce5f750575cf1ba3b
AapoKiiskila <aapo.kiiskila@gmail.com>
2024-10-13 21:00:43 +0300

    Added a function to colorChange.js

--- assets/javascript/colorChange.js
+++ assets/javascript/colorChange.js
@@ -1,6 +1,15 @@
 document.getElementById("colorButton").addEventListener("click", changeBackgroundColor);
+document.getElementById("colorInput").addEventListener("keydown", doNothing);
 
 function changeBackgroundColor()
 {
     document.body.style.background = document.getElementById("colorInput").value;
+}
+
+function doNothing(event)
+{
+    if (event.key == "Enter")
+    {
+        event.preventDefault();
+    }
 }
