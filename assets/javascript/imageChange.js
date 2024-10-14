6004ce3654f1b21d1c5059a9ee626359aba55faf
AapoKiiskila <aapo.kiiskila@gmail.com>
2024-10-14 23:46:21 +0300

    Added two images and imageChange.js. Also some other minor adjustments.

---
+++ assets/javascript/imageChange.js
@@ -0,0 +1,31 @@
+document.getElementById("nextButton").addEventListener("click", nextImage );
+document.getElementById("previousButton").addEventListener("click", previousImage );
+
+const images = ["assets/images/bank-image1.png", "assets/images/bank-image2.jpg"];
+let index = 0;
+
+document.getElementById("projects-img").src = images[index];
+
+function nextImage()
+{
+    index++;
+
+    if (index >= images.length)
+    {
+        index = images.length - 1;
+    }
+
+    document.getElementById("projects-img").src = images[index];
+}
+
+function previousImage()
+{
+    index--;
+
+    if (index <= 0)
+    {
+        index = 0;
+    }
+
+    document.getElementById("projects-img").src = images[index];
+}
