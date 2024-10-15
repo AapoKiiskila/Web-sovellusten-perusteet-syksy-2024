9307b9dc4e8c12fb49984de4a63124ce3f15633a
AapoKiiskila <aapo.kiiskila@gmail.com>
2024-10-15 19:07:31 +0300

    Added languages.js

---
+++ assets/javascript/languages.js
@@ -0,0 +1,18 @@
+class Language
+{
+    constructor(language, level)
+    {
+        this.language = language;
+        this.level = level;
+    }
+}
+
+const languages = [new Language("Finnish", "Native"), new Language("English", "Fluent"), new Language("Swedish", "Basics")];
+
+for (let i = 0; i < languages.length; i++)
+{
+    const object = languages[i];
+    const li = document.createElement("li");
+    li.innerText = object.language + " - " + object.level;
+    document.getElementById("languagesList").appendChild(li);
+}
