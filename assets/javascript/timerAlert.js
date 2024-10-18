5854b12edced2604864a5e24a30575f8842bab20
AapoKiiskila <aapo.kiiskila@gmail.com>
2024-10-18 14:20:52 +0300

    Added footer and another timer

--- assets/javascript/timerAlert.js
+++ assets/javascript/timerAlert.js
@@ -1,6 +1,22 @@
 setTimeout(thankYouAlert, 5000);
+setInterval(setFontWeight, 1000);
 
 function thankYouAlert()
 {
     alert("Thank you for visiting this web page!");
+}
+
+function setFontWeight()
+{
+    const box = document.getElementById("projects-GitHub");
+
+    if (box.style.fontWeight == "")
+    {
+        box.style.fontWeight = "bold";
+    }
+
+    else
+    {
+        box.style.fontWeight = "";
+    }
 }
